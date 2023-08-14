930ec19bf91900d6c18d1c33f1bc7ce20d606c08
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-13 22:43:03 -0400

    Clase sobre Promesas

---
+++ async/promises.js
@@ -0,0 +1,42 @@
+function hola(nombre){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            console.log("Hola " + nombre);
+            resolve(nombre);
+        },1500);
+    });
+}
+
+function hablar(nombre){
+    return new Promise((resolve,reject) => {
+        setTimeout(function(){
+                console.log("bla bla bla...");
+                //resolve(nombre);
+                reject("hay un error");
+            },1000);
+    });
+}
+
+function adios(nombre){
+    return new Promise((resolve,reject) => {
+        setTimeout(function(){
+            console.log("Adios "+ nombre);
+            resolve();
+        });
+    });
+
+}
+console.log("Inicnado el proceso");
+hola("Claudia")
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre)=>{
+        console.log("Terminado el proceso");
+    })
+    .catch(error => {
+        console.error("hay un error");
+        console.error(error);
+    });
