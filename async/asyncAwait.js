0a7ed4ecff9dc5b264236742e4aae9ad60513bc7
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-14 08:36:57 -0400

    Clase async away

--- async/asyncAwait.js
+++ async/asyncAwait.js
@@ -1,4 +1,4 @@
-function hola(nombre){
+async function hola(nombre){
     return new Promise(function(resolve,reject){
         setTimeout(function(){
             console.log("Hola " + nombre);
@@ -7,17 +7,17 @@ function hola(nombre){
     });
 }
 
-function hablar(nombre){
+async function hablar(nombre){
     return new Promise((resolve,reject) => {
         setTimeout(function(){
                 console.log("bla bla bla...");
-                //resolve(nombre);
-                reject("hay un error");
+                resolve(nombre);
+                //reject("hay un error");
             },1000);
     });
 }
 
-function adios(nombre){
+async function adios(nombre){
     return new Promise((resolve,reject) => {
         setTimeout(function(){
             console.log("Adios "+ nombre);
@@ -25,17 +25,15 @@ function adios(nombre){
         });
     });
 }
-console.log("Inicnado el proceso");
-hola("Claudia")
-    .then(hablar)
-    .then(hablar)
-    .then(hablar)
-    .then(hablar)
-    .then(adios)
-    .then((nombre)=>{
-        console.log("Terminado el proceso");
-    })
-    .catch(error => {
-        console.error("hay un error");
-        console.error(error);
-    });
+
+async function main(){
+    let nombre = await hola("Claudia");
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+}
+
+console.log("Empezamos el proceso");
+main();
+console.log("Termina el proceso");
