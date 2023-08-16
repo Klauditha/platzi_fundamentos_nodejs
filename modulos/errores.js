f6fc7e7d66cc2991cba9dc7107bd711ef2dc6ee6
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-16 15:24:49 -0400

    Clase try catch

---
+++ modulos/errores.js
@@ -0,0 +1,35 @@
+function otraFuncion(){
+    return seRompre();
+}
+
+function seRompre(){
+    return 3+z;
+}
+
+function seRompeAsincrona(cb){
+    setTimeout(function(){
+        try{
+            return 3+z;
+        }
+        catch(err){
+            console.error("Error en la funcion asincrona");
+            cb(err)
+        }
+        
+    })
+}
+try{
+    //seRompre();
+    //otraFuncion();
+    seRompeAsincrona(function(err){
+        console.log('hayerror: '+ err.message);
+    });
+}
+catch(err){
+    console.error("Vaya algo se ha roto");
+    console.error(err.message);
+    console.error(err.stack);
+    console.log("Lo hemos capturado");
+}
+
+console.log("este es el final");
